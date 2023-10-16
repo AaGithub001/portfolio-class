a44ad544cf70c88209e8e17ff55a6e06108ff324
AaGithub001 <aasigning001@gmail.com>
2023-10-16 15:20:46 +0545

    tailwind-practice

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  return  <nav className=" bg-blue-300 text-semibold height-15">
+    <h1 className=" bg-blue-300 w-full h-[60px] flex justify-center items-center text-semibold height-15 fixed bottom-0"> Designed By | Achyut Aryal </h1>
+  </nav>
+};
 
 export default Footer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <h1 className="text-xl font-bold underline">Hello world!</h1>;
+
+  return   <nav className=" w-full flex  bg-blue-300 text-black p-10 " >
+  <h1 className=" w-[50%] flex  justify-end ml-4 font-bold  text-1.5xl text-red-700">LOGO</h1>
+  <div className=" w-[50%] flex gap-[50px]">
+    <Link to ="/" className="font-semibold text-md text-red-500 hover:text-red-800 hover:border-b-2 hover:border-red-800"> Home</Link>
+    <Link to ="/about" className="font-semibold text-md text-red-500 hover:text-red-800 hover:border-b-2 hover:border-red-800"> About</Link>
+    <Link to ="/service" className="font-semibold text-md text-red-500 hover:text-red-800 hover:border-b-2 hover:border-red-800"> Service</Link>
+    <Link to ="/contact" className="font-semibold text-md text-red-500 hover:text-red-800 hover:border-b-2 hover:border-red-800"> Contact</Link>
+  </div>
+</nav>;
 };
 
 export default Header;
