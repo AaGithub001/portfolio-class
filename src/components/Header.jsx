2980d496b1668c62a31ad7bcf166e9190e0b528b
AaGithub001 <aasigning001@gmail.com>
2023-11-02 13:52:16 +0545

    formvalidating

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import {FaBars} from 'react-icons/fa'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Header = () => {
   const[isMobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -20,25 +20,29 @@ const Header = () => {
 
 <>
 <nav className=" w-full sm:flex hidden bg-blue-300 text-black p-10 ">
-  <h1 className=" w-[50%] flex  ml-4 font-bold  text-1.5xl text-red-700">LOGO</h1>
+  <h1 className=" w-[50%] flex px-4 py-4 font-bold  text-1.5xl text-red-700">LOGO</h1>
   <div className=" w-[50%] flex gap-[50px] ">
-    <Link to ="/" className={`font-semibold text-md text-red-500 hover:text-red-800 hover:border-b-2 hover:border-red-800 ${isActive('/')}`}> Home</Link>
-    <Link to ="/about" className={`font-semibold text-md text-red-500 hover:text-red-800 hover:border-b-2 hover:border-red-800 ${isActive('/about')}`}> About</Link>
-    <Link to ="/service" className={`font-semibold text-md text-red-500 hover:text-red-800 hover:border-b-2 hover:border-red-800 ${isActive('/service')}`}> Service</Link>
-    <Link to ="/contact" className={`font-semibold text-md text-red-500 hover:text-red-800 hover:border-b-2 hover:border-red-800${isActive('/contact')}`}> Contact</Link>
+    <Link to ="/" className={` font-bold text-1.5xl text-red-600  hover:text-red-800 hover:border-b-4 hover:border-red-800 ${isActive('/')}`}> Home</Link>
+    <Link to ="/about" className={`font-bold text-1.5xl text-red-600 hover:text-red-800 hover:border-b-4 hover:border-red-800 ${isActive('/about')}`}> About</Link>
+    <Link to ="/service" className={`font-bold text-1.5xl text-red-600 hover:text-red-800 hover:border-b-4 hover:border-red-800 ${isActive('/service')}`}> Service</Link>
+    <Link to ="/contact" className={`font-bold text-1.5xl text-red-600 hover:text-red-800 hover:border-b-4 hover:border-red-800 ${isActive('/contact')}`}> Contact</Link>
   </div>
 </nav>
-    <div className="sm:hidden flex justify-between px-4 pt-4 bg-red-300 h-[60px]">
-      <h1 className="mt-4 ml-4">LOGO</h1>
-      <button className="flex  mt-2" onClick={toggleMenu}><FaBars/></button>
-      </div>
+    <div className="sm:hidden flex justify-between items-center px-4  bg-cyan-300 h-[60px]">
+      <h1 className=" flex px-8 py-4 font-bold text-1.5xl ">Logo</h1>
+        {/* <button className="flex  mt-2" onClick={toggleMenu}>
+          {isMobileMenuOpen ?   
+            <FaTimes>:<FaBars/>
+         }
+        </button> */}
+    </div>
       {isMobileMenuOpen &&(
       <div className="flex flex-col gap-4 pb-4 bg-cyan-200 absolute top-0 h-screen w-[240px] px-8">
-       <Link to="/"  className="mx-4 my-4">Logo</Link>
-        <Link to="/" >Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contacts</Link>
-        <Link to="/service">Services</Link>
+       <Link to="/"  className="mx-4 my-4 font-bold text-1.5xl">Logo</Link>
+        <Link to="/" className="font-bold text-1.5xl">Home</Link>
+        <Link to="/about" className="font-bold text-1.5xl">About</Link>
+        <Link to="/contact" className="font-bold text-1.5xl">Contacts</Link>
+        <Link to="/service" className="font-bold text-1.5xl">Services</Link>
 
 
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,37 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Contact = () => {
+  const[firstname, setFirstname] = useState("");
+  const[lastname, setLastname] = useState("");
+  const[phone, setPhone] = useState("");
+  const[address, setAddress] = useState("");
+  const[email, setEmail] = useState("");
+  const[message,setMessage] = useState("");
+
+  console.log(firstname);
+  console.log(lastname);
+  console.log(phone);
+  console.log(address);
+  console.log(email);
+  console.log(message);
+
+  
+  
+  const handleClick = (e) => {
+  e.preventDefault();
+    if (firstname && lastname &&phone && address && email && message )
+    {
+    toast("form submit successfully");
+    setFirstname("");
+    setLastname("");
+    setPhone("");
+    setAddress("");
+    setEmail("");
+    setMessage("");
+  }
+  else if(!firstname) {
+  toast("Please fill the Firstname");
+  }
+  else if(!lastname) {
+    toast("Please fill the Lastname");
+    }
+  else if(!phone) {
+      toast("Please fill the Phone");
+      }
+  else if(!address) {
+        toast("Please fill the Address");
+        }
+  else if(!email) {
+          toast("Please fill the Email");
+          }
+  else if(!message) {
+            toast("Please fill the Message");
+            }
+  else
+  toast("Please fill the form completely");
+  }
+  
+
+
+
+
   return (
     
 <form className="bg-violet-200 mt-8 p-8 w-fit">
 <div className='grid grid-cols-2 gap-4'>
   <div className='flex flex-col'>
     <label className='mr-3 '>Firstname</label>
-    <input type="text" name='      firstname' placeholder='firstname' className='px-3 py-2 rounded-md  border-blue-400 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-700'/>
+    <input value={firstname} required onChange={e => setFirstname(e.target.value)} type="text" name='      firstname' placeholder='firstname' className='px-3 py-2 rounded-md  border-blue-400 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-700'/>
   </div>
   <div className='flex flex-col '>
     <label className='mr-3'>Lastname</label>
-    <input type="text" name='Lastname' placeholder='Lasttname' className='px-3 py-2 rounded-md border-blue-400 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-700' />
+    <input value={lastname}  required onChange={e => setLastname(e.target.value)} type="text" name='Lastname' placeholder='Lasttname' className='px-3 py-2 rounded-md border-blue-400 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-700' />
   </div>
   <div className='flex flex-col '>
     <label htmlFor="firstname">Phone</label>
-    <input type="number" name='phone' placeholder='Phone no.' className='px-3 py-2 rounded-md  border-blue-400 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-700' />
+    <input  value={phone} required onChange={e => setPhone(e.target.value)}type="number" name='phone' placeholder='Phone no.' className='px-3 py-2 rounded-md  border-blue-400 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-700' />
   </div>
   <div className='flex flex-col '>
     <label htmlFor="firstname">Address</label>
-    <input type="text" name='address' placeholder='address' className='px-3 py-2 rounded-md  border-blue-400 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-700'/>
+    <input  value={address} required onChange={e => setAddress(e.target.value)} type="text" name='address' placeholder='address' className='px-3 py-2 rounded-md  border-blue-400 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-700'/>
   </div>
 </div>
 <div className='mt-4 flex gap-2'>
     <label htmlFor="Email">Email</label>
-    <input type="email" name='Email' placeholder=' Email'  className='px-3 py-2 rounded-md  border-blue-400 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-700'/>
+    <input value={email} required onChange={e => setEmail(e.target.value)} type="email" name='Email' placeholder=' Email'  className='px-3 py-2 rounded-md  border-blue-400 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-700'/>
   </div>
   <div className='mt-4 flex flex-row gap-2 items-center'>
     <label htmlFor="Message">Message</label>
-    <textarea name="message" id="" cols="30" rows="5" className='px-3 py-2 rounded-md  border-blue-400 bg-gray-50 focus:outline-none'></textarea>
+    <textarea value={message} required onChange={e => setMessage(e.target.value)} name="message" id="" cols="30" rows="5" className='px-3 py-2 rounded-md  border-blue-400 bg-gray-50 focus:outline-none'></textarea>
   </div>
   <div className='flex justify-center'>
-  <button className='bg-yellow-500 mt-4 px-4 py-4 rounded-md text-black font-semibold'> Submit</button>
+  <ToastContainer />
+  <button onClick={handleClick} className='bg-yellow-500 mt-4 px-4 py-4 rounded-md text-black font-semibold'> Submit</button>
   </div>
 </form>
 
